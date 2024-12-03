d559efb795b304f44da8a407b6e2a01da6b15d10
leeseungeon92 <oksktmddjs@gmail.com>
2024-12-04 03:31:09 +0900

    프론트엔드 로그인 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -16,8 +16,19 @@ function login() {
         method : "POST",
         headers : {
             "Content-Type" : "application/json",
-            
         },
         body : JSON.stringify(req)
     })
+        .then((res) => res.json())
+        .then((res) => 
+            {
+                if(res.success){
+                    location.href = "/";
+                } else{
+                    alert(res.msg);
+                }
+            })
+            .catch((err) => {
+                console.error("로그인 중 에러 발생");
+            });
 }
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,15 +1,43 @@
 "use strict";
 
-const hello = (req, res) => {
-    // 기능
-    res.render("home/index")
-}
+const users = {
+    id : ["oksktmddjs", "tmddjs11"],
+    password : ["gudwns11", "0119962"]
+};
 
-const login = (req,res) => {
-    res.render("home/login")
-}
+const output = { 
+    home : (req, res) => {
+        // 기능
+        res.render("home/index")
+    },
+    
+    login : (req,res) => {
+        res.render("home/login")
+    }
+};
+
+const process = {
+    login : (req, res) => {
+        const id = req.body.id,
+        password = req.body.password;
+        
+        if (users.id.includes(id)){
+            const idx = users.id.indexOf(id);
+            if(users.password[idx] === password){
+                return res.json({
+                    success : true,
+                });
+            }
+        }
+
+        return res.json({
+            success : false,
+            msg : "로그인에 실패하였습니다."
+        })
+    }
+};
 
 module.exports = {
-    hello,
-    login
+    output,
+    process
 };
