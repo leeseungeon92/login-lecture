eb184d80a1e666e5fe6b145d8bee7e6da475a8b7
leeseungeon92 <oksktmddjs@gmail.com>
2024-12-04 01:41:52 +0900

    프런트 단 javascript 파일 만들기

--- app/app.js
+++ app/app.js
@@ -9,5 +9,6 @@ app.set("view engine", "ejs");
 
 const home = require("./src/routes/home");
 app.use("/", home); // use -> 미들 웨어를 등록해주는 메서드.
+app.use(express.static(`${__dirname}/src/public`))
 
 module.exports = app;
