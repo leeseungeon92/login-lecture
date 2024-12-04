b3db689c53e756ae2f1cee31dd6e8e53312e860c
leeseungeon92 <oksktmddjs@gmail.com>
2024-12-04 13:25:08 +0900

    fs모듈을 통한 로그인 기능 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -1,41 +1,47 @@
 "use strict";
 
+const fs = require("fs").promises;
+
 class UserStorage {
-    static #users = {
-        id : ["oksktmddjs", "tmddjs11"],
-        password : ["gudwns11", "0119962"],
-        name : ["이승언", "나개발"]
-    };
+  static getUsers(...fields) {
+    //const users = this.#users;
+    const newUsers = fields.reduce((newUsers, field) => {
+      if (users.hasOwnProperty(field)) {
+        newUsers[field] = users[field];
+      }
+      return newUsers;
+    }, {});
+    return newUsers;
+  }
 
-    static getUsers(...fields) {
-        const users = this.#users;
-        const newUsers = fields.reduce((newUsers, field) => {
-            if(users.hasOwnProperty(field)){
-                newUsers[field] = users[field];
-            } return newUsers;
-        }, {});
-        return newUsers;
-    }
+  static getUserInfo(id) {
+    return fs
+        .readFile("./src/databases/users.json")
+        .then((data) => {
+            return this.#getUserInfo(data, id);
+    })
+    .catch(console.error);
+  }
 
-    static getUserInfo(id){
-        const users = this.#users;
-        const idx = users.id.indexOf(id); // => 0
-        const usersKeys = Object.keys(users); // => [id, password, name]
-        const userInfo = usersKeys.reduce((newUsers, info) => {
-            newUsers[info] = users[info][idx];
-            return newUsers; // => oksktmddjs, gudwns11, 이승언
-        }, {});
+  static save(userInfo) {
+    //const users = this.#users;
+    users.id.push(userInfo.id);
+    users.password.push(userInfo.password);
+    users.name.push(userInfo.name);
+    return { success: true };
+  }
 
-        return userInfo;
-    }
+  static #getUserInfo(data, id) {
+    const users = JSON.parse(data);
+            const idx = users.id.indexOf(id); // => 0
+            const usersKeys = Object.keys(users); // => [id, password, name]
+            const userInfo = usersKeys.reduce((newUsers, info) => {
+                newUsers[info] = users[info][idx];
+                return newUsers; // => oksktmddjs, gudwns11, 이승언
+    }, {});
 
-    static save(userInfo){
-        const users = this.#users;
-        users.id.push(userInfo.id);
-        users.password.push(userInfo.password);
-        users.name.push(userInfo.name);
-        return {success:true};
-    }
+      return userInfo;
+  }
 }
 
 module.exports = UserStorage;
