525849651619ce665f8fd6baede8a6e7a894806c
leeseungeon92 <oksktmddjs@gmail.com>
2024-12-06 16:49:54 +0900

    백엔드 맛보기 최종 최적화

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -6,7 +6,7 @@ class UserStorage {
 
   static getUserInfo(id) {
     return new Promise((resolve, reject)=>{
-      const query = "SELECT * FROM asdadss where id = ?;";
+      const query = "SELECT * FROM users where id = ?;";
       db.query(query, [id], (err, data)=>{
         if(err) reject(`${err}`);
         else resolve(data[0]);
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -21,25 +21,33 @@ const output = {
 };
 
 const process = {
+
     login : async (req, res) => {
         const user = new User(req.body);
         const response = await user.login();
-        if(response.err) logger.error(`POST / login 200 Response : "success : ${response.success}, msg : ${response.err}"`)
-        logger.info(
-            `POST / login 200 Response : "success : ${response.success}, msg : ${response.msg}"`
-        );
-        return res.json(response);
+
+        const url = {
+            method : "POST",
+            path : "/login",
+            status : response.err ? 400 : 200
+        };
+
+        log(response, url);
+        return res.status(url.status).json(response);
     },
 
     register : async(req, res) => {
         const user = new User(req.body);
         const response = await user.register();
-        if(response.err) logger.error(`POST / login 200 Response : "success : ${response.success}, msg : ${response.err}"`)
-        else 
-        logger.info(
-            `POST / register 200 Response : "success : ${response.success}, msg : ${response.msg}"`
-        );
-        return res.json(response);
+
+        const url = {
+            method : "POST",
+            path : "/register",
+            status : response.err ? 409 : 201
+        };
+
+        log(response, url);
+        return res.status(url.status).json(response);
 
     }
 }
@@ -48,3 +56,15 @@ module.exports = {
     output,
     process
 };
+
+const log = (response, url) => {
+    if(response.err) {
+        logger.error(
+        `${url.method} / ${url.path} ${url.status} Response : ${response.success} ${response.err}"`
+        )
+    } else {
+        logger.info(
+            `${url.method} / ${url.path} ${url.status} Response : ${response.success} ${response.msg || ""}`
+        );
+    }
+}
